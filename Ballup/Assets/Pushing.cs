2c47d6b5ef61a79637b4145400b05cda9163875e
Yagufrix <antonio.sein@g-tec.io>
2018-11-26 16:59:51 -0600

    stick
    stick

---
+++ Ballup/Assets/Pushing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pushing : MonoBehaviour {
+
+	public float moveSpeed;
+	public float yForce;
+	public float xForce;
+	public float dampenForce;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Vertical") * moveSpeed;
+		transform.Translate(Vector3.right * h);
+
+		if(Input.GetKey(KeyCode.Space)){
+			xForce += 5.0f;
+		}
+
+		if(Input.GetKeyUp (KeyCode.Space)){
+			
+		}
+	}
+}
